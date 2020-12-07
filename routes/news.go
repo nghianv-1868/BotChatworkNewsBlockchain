6b29a162210a95dfbbd8bdaf0db80bf24ebdd4b8
nghianv-1868 <ngo.van.nghia@sun-asterisk.com>
2020-12-07 19:33:29 +0700

    complete-change-follow-and-tags

--- routes/news.go
+++ routes/news.go
@@ -16,10 +16,14 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-type Post struct {
-	Tag     string `json:"tag"`
-	Link    string `json:"link"`
-	Created string `json:"created"`
+type InfoUser struct {
+	Data DataInfoUser `json:"data"`
+}
+
+type DataInfoUser struct {
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
 }
 
 //Struct of WebhookChatwork
@@ -83,46 +87,123 @@ func HandleChatworkWebhook(w http.ResponseWriter, r *http.Request, _ httprouter.
 		fmt.Println("An error occured: " + err.Error())
 	}
 	mess := string(load.Webhook_event.Body)
+
 	if strings.Contains(mess, "add_tag=") {
-		messTags := strings.Split(mess, "add_tag=")[1]
-		messTags = strings.ReplaceAll(messTags, " ", "")
-		tags := strings.Split(messTags, ",")
-		readData, err := ioutil.ReadFile(".following")
-		if err != nil {
-			fmt.Println(err)
-		}
-		arrData := strings.Split(string(readData), ",")
-
-		for _, v := range tags {
-			if !containsArrayString(arrData, v) {
-				arrData = append(arrData, v)
-			}
-		}
-		dataSave := []byte(strings.Join(arrData, ","))
-		err1 := ioutil.WriteFile(".following", dataSave, 0777)
-		if err1 != nil {
-			fmt.Println(err1)
-		}
+		messBody := strings.Split(mess, "add_tag=")[1]
+		messBody = strings.ReplaceAll(messBody, " ", "")
+		listTags := strings.Split(messBody, ",")
+		messRes := `[info][title]Tags[/title]- Add success: ` + strings.Join(listTags, " , ") + `[/info]`
+		sendMessage(messRes)
+		addToFile(listTags, ".tags")
 	} else if strings.Contains(mess, "remove_tag=") {
-		messTags := strings.Split(mess, "remove_tag=")[1]
-		messTags = strings.ReplaceAll(messTags, " ", "")
-		tags := strings.Split(messTags, ",")
+		messBody := strings.Split(mess, "remove_tag=")[1]
+		messBody = strings.ReplaceAll(messBody, " ", "")
+		listTags := strings.Split(messBody, ",")
+		readData, err := ioutil.ReadFile(".tags")
+		listTagsSuccess := []string{}
+		listTagsNotContains := []string{}
+		if err != nil {
+			fmt.Println(err)
+		}
+		for _, v := range listTags {
+			if containsArrayString(strings.Split(string(readData), ","), v) {
+				listTagsSuccess = append(listTagsSuccess, v)
+			} else {
+				listTagsNotContains = append(listTagsNotContains, v+"(not contains tags)")
+			}
+		}
+		messRes := `[info][title]Tags[/title]- Remove success: ` + strings.Join(listTagsSuccess, " , ") + `
+- Remove error: ` + strings.Join(listTagsNotContains, " , ") + `[/info]`
+		sendMessage(messRes)
+		removbeToFile(listTags, ".tags")
+	} else if strings.Contains(mess, "add_follow=") {
+		messBody := strings.Split(mess, "add_follow=")[1]
+		messBody = strings.ReplaceAll(messBody, " ", "")
+		listUsername := strings.Split(messBody, ",")
+		listFollow := []string{}
+		listFollowUsername := []string{}
+		listNotFound := []string{}
+		for _, v := range listUsername {
+			if getUserIdFromUsername(v) != "" {
+				listFollow = append(listFollow, getUserIdFromUsername(v))
+				listFollowUsername = append(listFollowUsername, v)
+			} else {
+				listNotFound = append(listNotFound, v)
+			}
+		}
+
+		messRes := `[info][title]Follow[/title]- Add success: ` + strings.Join(listFollowUsername, " , ") + `
+- Add error: ` + strings.Join(listNotFound, " , ") + `[/info]`
+		sendMessage(messRes)
+		addToFile(listFollow, ".following")
+	} else if strings.Contains(mess, "remove_follow=") {
+		messBody := strings.Split(mess, "remove_follow=")[1]
+		messBody = strings.ReplaceAll(messBody, " ", "")
+		listRemove := strings.Split(messBody, ",")
+		listUserIdRemove := []string{}
+		listUsernameRemove := []string{}
+		listUsernameNotFound := []string{}
 		readData, err := ioutil.ReadFile(".following")
 		if err != nil {
 			fmt.Println(err)
 		}
-		arrData := strings.Split(string(readData), ",")
+		for _, v := range listRemove {
+			if getUserIdFromUsername(v) != "" {
+				if !containsArrayString(strings.Split(string(readData), ","), getUserIdFromUsername(v)) {
+					listUsernameNotFound = append(listUsernameNotFound, v+"(not contains following)")
+				} else {
+					listUserIdRemove = append(listUserIdRemove, getUserIdFromUsername(v))
+					listUsernameRemove = append(listUsernameRemove, v)
+				}
 
-		for _, v := range tags {
-			if index := indexContainsArrayString(arrData, v); index != -1 {
-				arrData = append(arrData[:index], arrData[index+1:]...)
+			} else {
+				listUsernameNotFound = append(listUsernameNotFound, v+"(not found)")
 			}
 		}
-		dataSave := []byte(strings.Join(arrData, ","))
-		err1 := ioutil.WriteFile(".following", dataSave, 0777)
-		if err1 != nil {
-			fmt.Println(err1)
+		messRes := `[info][title]Follow[/title]- Remove success: ` + strings.Join(listUsernameRemove, " , ") + `
+- Remove error: ` + strings.Join(listUsernameNotFound, " , ") + `[/info]`
+		sendMessage(messRes)
+		removbeToFile(listUserIdRemove, ".following")
+	}
+}
+
+func addToFile(_list []string, _nameFile string) {
+	readData, err := ioutil.ReadFile(_nameFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	arrData := []string{}
+	if string(readData) != "" {
+		arrData = strings.Split(string(readData), ",")
+	}
+	for _, v := range _list {
+		if !containsArrayString(arrData, v) {
+			arrData = append(arrData, v)
 		}
+	}
+
+	dataSave := []byte(strings.Join(arrData, ","))
+	err1 := ioutil.WriteFile(_nameFile, dataSave, 0777)
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+}
+func removbeToFile(_list []string, _nameFile string) {
+	readData, err := ioutil.ReadFile(_nameFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	arrData := strings.Split(string(readData), ",")
+
+	for _, v := range _list {
+		if index := indexContainsArrayString(arrData, v); index != -1 {
+			arrData = append(arrData[:index], arrData[index+1:]...)
+		}
+	}
+	dataSave := []byte(strings.Join(arrData, ","))
+	err1 := ioutil.WriteFile(_nameFile, dataSave, 0777)
+	if err1 != nil {
+		fmt.Println(err1)
 	}
 }
 
@@ -146,23 +227,63 @@ func indexContainsArrayString(s []string, str string) int {
 	return -1
 }
 
-// func RemoveDuplicates(s []string) []string {
-// 	m := make(map[string]bool)
-// 	for _, item := range s {
-// 		if _, ok := m[item]; ok {
-// 			// duplicate item
-// 			fmt.Println(item, "is a duplicate")
-// 		} else {
-// 			m[item] = true
-// 		}
-// 	}
+func getUserIdFromUsername(_username string) string {
+	// URL request
+	reqURL := "https://api.twitter.com/2/users/by/username/" + _username
 
-// 	var result []string
-// 	for item, _ := range m {
-// 		result = append(result, item)
-// 	}
-// 	return result
-// }
+	token := os.Getenv("BEARER_TOKEN")
+
+	client := &http.Client{}
+
+	request, err := http.NewRequest("GET", reqURL, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	request.Header.Set("Authorization", "Bearer "+token)
+
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	var info InfoUser
+	error := json.Unmarshal(body, &info)
+	if error != nil {
+		return ""
+	}
+	return info.Data.Id
+}
+func getUsernameFromUserId(_userId string) string {
+	// URL request
+	reqURL := "https://api.twitter.com/2/users/" + _userId
+
+	token := os.Getenv("BEARER_TOKEN")
+
+	client := &http.Client{}
+
+	request, err := http.NewRequest("GET", reqURL, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	request.Header.Set("Authorization", "Bearer "+token)
+
+	resp, err := client.Do(request)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	var info InfoUser
+	error := json.Unmarshal(body, &info)
+	if error != nil {
+		return ""
+	}
+	fmt.Println(info)
+	return info.Data.Username
+}
 
 // func ValidateRequestChatwork(r *http.Request) {
 // 	err := godotenv.Load()
